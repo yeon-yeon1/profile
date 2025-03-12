379e2b75513d8746e666674473de0a005e56ebca
yeon-yeon1 <nojk0106@naver.com>
2025-03-13 05:43:12 +0900

    test

--- profile/src/App.jsx
+++ profile/src/App.jsx
@@ -5,7 +5,7 @@ import Profile from "./pages/Profile";
 function App() {
   return (
     <Routes>
-      <Route path="/profile" element={<Profile />} />
+      <Route path="/" element={<Profile />} />
     </Routes>
   );
 }
